2e72aa159719e38933a2a20c58f44ebc3ae03e8f
daviespaul <daviespaul@users.noreply.github.com>
2022-07-02 18:07:00 +0100

    Update streamlit_app.py
    re-order

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
-
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -22,9 +19,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+# display the table on the page 
+streamlit.dataframe(fruits_to_show)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
-
-# display the table on the page 
-streamlit.dataframe(fruits_to_show)
